c8f3ef20dab3bee8a2aa50302f4374f6301366eb
vishal11u <shitolevishal29@gmail.com>
2025-07-15 06:16:50 -0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
 import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
-import Footer from './sections/Footer';
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
       <Experiences />
       <Testimonial />
       <Contact />
-      <Footer/>
+      <Footer />
     </div>
   );
 };
