0dc50f8eeb5cc635caf2e52809be10a94c076d24
vishal11u <shitolevishal29@gmail.com>
2025-09-13 12:41:18 +0530

    Remove footer links and update copyright name

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,9 +4,7 @@ const Footer = () => {
     <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
       <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
       <div className="flex gap-2">
-        <p>Terms & Conditions</p>
-        <p>|</p>
-        <p>Privacy Policy</p>
+        
       </div>
       <div className="flex gap-5">
         {mySocials.map((social, index) => (
@@ -15,7 +13,7 @@ const Footer = () => {
           </a>
         ))}
       </div>
-      <p>© 2025 Ali. All rights reserved.</p>
+      <p>© 2025 Vishal. All rights reserved.</p>
     </section>
   );
 };
