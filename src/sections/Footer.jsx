2e122442316303d94189befe0b9df781ced1cb4d
vishal11u <shitolevishal29@gmail.com>
2025-09-13 12:46:00 +0530

    Refactor Footer component layout for improved structure and spacing

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,11 +1,9 @@
 import { mySocials } from "../constants";
 const Footer = () => {
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+    <section className="flex flex-col items-center justify-center gap-3 pb-5 text-sm text-neutral-400 c-space">
       <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
-      <div className="flex gap-2">
-        
-      </div>
+
       <div className="flex gap-5">
         {mySocials.map((social, index) => (
           <a href={social.href} key={index}>
@@ -13,6 +11,7 @@ const Footer = () => {
           </a>
         ))}
       </div>
+
       <p>© 2025 Vishal. All rights reserved.</p>
     </section>
   );
